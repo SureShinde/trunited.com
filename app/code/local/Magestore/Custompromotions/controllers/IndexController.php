051c2a60babe6208e3193ece2999b3d443dd8fe3
anthony <longvuxuan1989@gmail.com>
2017-02-25 22:59:22 +0700

    Update code the mobile #

--- app/code/local/Magestore/Custompromotions/controllers/IndexController.php
+++ app/code/local/Magestore/Custompromotions/controllers/IndexController.php
@@ -48,10 +48,14 @@ class Magestore_Custompromotions_IndexController extends Mage_Core_Controller_Fr
 		{
 			if($customer->getPhoneNumber() != null){
 				$check++;
-				zend_debug::dump($customer->getId().' - '.$customer->getPhoneNumber());
 				$phone = Mage::helper('custompromotions/verify')->formatPhoneToDatabase($customer->getPhoneNumber());
-//				zend_debug::dump($phone);
-				$customer->setPhoneNumber($phone);
+				zend_debug::dump($customer->getPhoneNumber());
+				if(strpos($customer->getPhoneNumber(),'-') > 0){
+					zend_debug::dump($phone);
+					zend_debug::dump($customer->getId().' - '.$customer->getPhoneNumber());
+					$customer->setPhoneNumber($phone);
+					$customer->save();
+				}
 			} else {
 				$_check++;
 				$customer->setPhoneNumber('');
